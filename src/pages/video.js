eff2653cee1f6370625fb7876b47469de3a8ce01
Alberto Azevedo <garfolino@me.com>
2023-06-15 11:56:00 +0200

    second

---
+++ src/pages/video.js
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+  return (
+    <div className="video">
+      <video
+        className="video_player"
+        ref={videoRef}
+        onClick={handleStart}
+        src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+        loop
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
